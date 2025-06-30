ad338aca199c821c4516601e5f14940aa6e81b94
shivanshsrivastava29 <shivanshsrivastava2911@gmail.com>
2025-06-30 20:01:12 +0530

    Add project files.

---
+++ StudentRecordApp/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRecordApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Roll Number")]
+        public string RollNumber { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Range(1, 8)]
+        public int Semester { get; set; }
+    }
+}
